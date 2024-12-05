33710a96dcd235259b01558e38011e0768f1e3ad
coltuhlenhopp <97199744+coltuhlenhopp@users.noreply.github.com>
2024-12-05 15:27:19 -0700

    Update tac.h

--- FinalCompiler/tac.h
+++ FinalCompiler/tac.h
@@ -1,145 +1,141 @@
-// Set of functions to emit IR code
-FILE * IRcode;
-FILE * dataIR;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void  initIRcodeFile(){
-    // Creates an IR file with a generic header that needs to be in every file
+// File pointers for IR code and data sections
+FILE *IRcode;
+FILE *dataIR;
 
+// Initializes the IR code file with a generic header
+void initIRcodeFile() {
     IRcode = fopen("IRcode.ir", "w");
     dataIR = fopen("dataIR.ir", "w");
-    
-    //fprintf(IRcode, ".text\n");
+
+    // Add main section header
     fprintf(IRcode, "MAIN:\n");
     fprintf(IRcode, "# -----------------------\n");
 
-    // data
+    // Add data section header
     fprintf(dataIR, "DATA:\n");
-    //fprintf(dataIR, "newLOADne: .asciiz \"\\n\"\n");
-
 
     fclose(IRcode);
     fclose(dataIR);
-
 }
 
-void emitIRAssignment(char * id1, char * id2){
+// Emits IR code for an assignment operation
+void emitIRAssignment(char *id1, char *id2) {
     IRcode = fopen("IRcode.ir", "a");
 
-    fprintf(IRcode, "LOAD T1,%s\n", id1);
-    fprintf(IRcode, "LOAD T2,%s\n", id2);
-    fprintf(IRcode, "LOAD T2, T1\n");
+    fprintf(IRcode, "LOAD T1,%s\n", id1);  // Load first variable into T1
+    fprintf(IRcode, "LOAD T2,%s\n", id2);  // Load second variable into T2
+    fprintf(IRcode, "LOAD T2, T1\n");      // Assign T1's value to T2
 
     fclose(IRcode);
 }
 
-void emitIRConstantIntAssignment (int id1, char id2[50]){
+// Emits IR code for assigning a constant integer to a variable
+void emitIRConstantIntAssignment(int id1, char id2[50]) {
     IRcode = fopen("IRcode.ir", "a");
 
-    fprintf(IRcode, "LOAD T%d,%s\n", id1, id2);
+    fprintf(IRcode, "LOAD T%d,%s\n", id1, id2);  // Assign constant value to Tn
 
     fclose(IRcode);
 }
 
-void emitIRWriteInt(int n){
+// Emits IR code for writing an integer value to the output
+void emitIRWriteInt(int n) {
     IRcode = fopen("IRcode.ir", "a");
 
     fprintf(IRcode, "# Printing -----------\n");
-    fprintf(IRcode, "LOAD V0, 1\n");
-    fprintf(IRcode, "LOAD A0, %d\n", n);
+    fprintf(IRcode, "LOAD V0, 1\n");      // Syscall for integer print
+    fprintf(IRcode, "LOAD A0, %d\n", n);  // Load the integer into A0
     fprintf(IRcode, "SYSTEM CALL\n");
 
     fclose(IRcode);
 }
 
-void emitIRWriteId(char id[50], char type[5]){
+// Emits IR code for writing a variable's value to the output
+void emitIRWriteId(char id[50], char type[5]) {
     IRcode = fopen("IRcode.ir", "a");
 
     fprintf(IRcode, "# Printing -----------\n");
 
-    //if id is an int
     if (strcmp(type, "int") == 0) {
+        // Print integer
         fprintf(IRcode, "LOAD V0, 1\n");
         fprintf(IRcode, "LOAD WORD: A0, %s\n", id);
-    } 
-    // if id is a char
-    else { 
+    } else {
+        // Print character
         fprintf(IRcode, "LOAD V0, 4\n");
         fprintf(IRcode, "LOAD AD: A0, %s\n", id);
     }
-    fprintf(IRcode, "SYSTEM CALL\n");    
+    fprintf(IRcode, "SYSTEM CALL\n");
 
     fclose(IRcode);
 }
 
-void emitIRArrayDecl (char id[50], int size) {
+// Emits IR code for array declaration
+void emitIRArrayDecl(char id[50], int size) {
     dataIR = fopen("dataIR.ir", "a");
 
-    // int bits = size*4;
-    fprintf(dataIR, "%s  BLOCK   %d", id, size);
+    fprintf(dataIR, "%s  BLOCK   %d\n", id, size);  // Declare block for array
 
     fclose(dataIR);
 }
 
-void emitIRCharDecl (char id[50], char c) {
+// Emits IR code for character declaration
+void emitIRCharDecl(char id[50], char c) {
     dataIR = fopen("dataIR.ir", "a");
 
-    fprintf(dataIR, "%s  BYTE   \'%c\'", id, c);
+    fprintf(dataIR, "%s  BYTE   \'%c\'\n", id, c);  // Declare single character
 
     fclose(dataIR);
 }
 
-void emitEndOfAssemblyCodeIR(){
-    IRcode = fopen("IRcode.ir", "a ");
+// Emits IR code to terminate the program
+void emitEndOfAssemblyCodeIR() {
+    IRcode = fopen("IRcode.ir", "a");
 
     fprintf(IRcode, "# -----------------\n");
     fprintf(IRcode, "#  Done, terminate program.\n\n");
-    // fprintf(IRcode, "LOAD V0,1   # call code for terminate\n");
-    // fprintf(IRcode, "SYSTEM CALL      # system call (terminate)\n");
-    fprintf(IRcode, "LOAD V0,10   # call code for terminate\n");
-    fprintf(IRcode, "SYSTEM CALL      # system call (terminate)\n");
+    fprintf(IRcode, "LOAD V0,10   # Syscall code for termination\n");
+    fprintf(IRcode, "SYSTEM CALL\n");
     fprintf(IRcode, "END MAIN\n");
 
-    fclose(IRcode);    
+    fclose(IRcode);
 }
 
+// Emits IR code for an integer variable declaration
 void emitIntVarIR(char id[50], int val) {
+    dataIR = fopen("dataIR.ir", "a");
 
-    dataIR = fopen("dataIR.ir", "a ");
+    fprintf(dataIR, "%s:   WORD  %d\n", id, val);  // Declare integer variable
 
-    fprintf(dataIR, "%s:   WORD  %d\n", id, val);
-
-    fclose(dataIR); 
-
+    fclose(dataIR);
 }
 
+// Function to emit a new function's IR code (placeholder)
 void emitNewFuncIR(char func[10]) {
-
-
-
+    // Implementation pending
 }
 
+// Appends IR code from "IRcode.ir" into "dataIR.ir"
 void addMainToDataIR() {
-
     IRcode = fopen("IRcode.ir", "a+");
     dataIR = fopen("dataIR.ir", "a+");
-    //funcs = fopen("funcs.ir", "a+");
- 
-    // If file is not found then return.
-    if (!IRcode && !dataIR) {
-        printf("Unable to open/"
-               "detect file(s)\n");
+
+    // Ensure files are successfully opened
+    if (!IRcode || !dataIR) {
+        printf("Unable to open/detect file(s)\n");
         return;
     }
- 
+
     char buf[100];
- 
-    // expLOADcitly writing "\n"
-    // to the destination file
-    // so to enhance readabiLOADty.
+
+    // Add a newline for readability
     fprintf(dataIR, "\n");
- 
-    // writing the contents of
-    // source file to destination file.
+
+    // Write contents of IRcode to dataIR
     while (!feof(IRcode)) {
         fgets(buf, sizeof(buf), IRcode);
         fprintf(dataIR, "%s", buf);
@@ -147,17 +143,6 @@ void addMainToDataIR() {
 
     fprintf(dataIR, "\n");
 
-    // while (!feof(funcs)) {
-    //     fgets(buf, sizeof(buf), funcs);
-    //     fprintf(dataIR, "%s", buf);
-    // }
- 
-    // rewind(dataIR);
- 
-    // printing contents of
-    // destination file to stdout.
-    // while (!feof(dataIR)) {
-    //     fgets(buf, sizeof(buf), dataIR);
-    //     printf("%s", buf); //Prints result to console if needed
-    // }
-}   
+    fclose(IRcode);
+    fclose(dataIR);
+}
