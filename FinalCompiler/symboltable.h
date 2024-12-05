322d4e92287c7af9b6e930bbc722324380bf5084
coltuhlenhopp <97199744+coltuhlenhopp@users.noreply.github.com>
2024-12-05 15:14:26 -0700

    Update symboltable.h

--- FinalCompiler/symboltable.h
+++ FinalCompiler/symboltable.h
@@ -1,195 +1,162 @@
-
-struct Entry
-{
-	int itemID;
-	char itemName[50];  //the name of the identifier
-	char itemKind[8];  //is it a function or a variable?
-	char itemType[8];  // Is it int, char, etc.?
-    char itemValue[25];
-	char scope[50];     // global, or the name of the function
-	int itemUsed;
+struct Entry {
+    int itemID;                // Unique identifier for the symbol
+    char itemName[50];         // Name of the identifier
+    char itemKind[8];          // Specifies if it's a function or variable
+    char itemType[8];          // Data type (int, char, etc.)
+    char itemValue[25];        // Value assigned to the identifier
+    char scope[50];            // Scope of the identifier (e.g., global or function name)
+    int itemUsed;              // Flag indicating if the identifier is used
 };
 
-struct Entry symTabItems[100];
-int symTabIndex = 0;
-int SYMTAB_SIZE = 20;
+struct Entry symTabItems[100]; // Array to hold symbol table entries
+int symTabIndex = 0;           // Tracks the current index in the symbol table
+int SYMTAB_SIZE = 20;          // Maximum size of the symbol table
 
-void symTabAccess(void){
-	printf("::::> Symbol table accessed.\n\n");
-}
-
-void addItem(char itemName[50], char itemKind[8], char itemType[8], char scope[50]){
-	
-
-		// what about scope? should you add scope to this function?
-		symTabItems[symTabIndex].itemID = symTabIndex;
-		strcpy(symTabItems[symTabIndex].itemName, itemName);
-		strcpy(symTabItems[symTabIndex].itemKind, itemKind);
-		strcpy(symTabItems[symTabIndex].itemType, itemType);
-        strcpy(symTabItems[symTabIndex].itemValue, "NULL");
-		strcpy(symTabItems[symTabIndex].scope, scope);
-		symTabItems[symTabIndex].itemUsed = 0;
-		symTabIndex++;
-	
+void addItem(char itemName[50], char itemKind[8], char itemType[8], char scope[50]) {
+    // Adds a new item to the symbol table
+    symTabItems[symTabIndex].itemID = symTabIndex;
+    strcpy(symTabItems[symTabIndex].itemName, itemName);
+    strcpy(symTabItems[symTabIndex].itemKind, itemKind);
+    strcpy(symTabItems[symTabIndex].itemType, itemType);
+    strcpy(symTabItems[symTabIndex].itemValue, "NULL");
+    strcpy(symTabItems[symTabIndex].scope, scope);
+    symTabItems[symTabIndex].itemUsed = 0;
+    symTabIndex++;
 }
 
 void showSymTable() {
-	printf("itemID    itemName    itemKind    itemType  itemValue  itemSope    itemUsed?\n");
-	printf("----------------------------------------------------------------------------\n");
-	for (int i=0; i<symTabIndex; i++){
-		printf("%5d  %10s  %10s  %10s %10s %10s %10d \n",symTabItems[i].itemID, symTabItems[i].itemName, symTabItems[i].itemKind, symTabItems[i].itemType, symTabItems[i].itemValue, symTabItems[i].scope, symTabItems[i].itemUsed);
-	}	
-
-	printf("----------------------------------------------------------------------------\n\n");
+    // Displays all entries in the symbol table
+    printf("    ID        Name        Kind        Type      Value      Scope    \n");
+    printf("----------------------------------------------------------------------------\n");
+    for (int i = 0; i < symTabIndex; i++) {
+        printf("%5d  %10s  %10s  %10s %10s %10s \n", 
+               symTabItems[i].itemID, 
+               symTabItems[i].itemName, 
+               symTabItems[i].itemKind, 
+               symTabItems[i].itemType, 
+               symTabItems[i].itemValue, 
+               symTabItems[i].scope);
+    }
+    printf("----------------------------------------------------------------------------\n");
 }
 
 int found(char itemName[50], char scope[50]) {
-    // Lookup an identifier in the symbol table
-    // what about scope?
-    // return TRUE or FALSE
-    // Later on, you may want to return additional information
-
-    // Dirty loop, becuase it counts SYMTAB_SIZE times, no matter the size of the symbol table
-
-    for(int i=0; i < SYMTAB_SIZE; i++){
-
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-
-        int str1 = strcmp(symTabItems[i].itemName, itemName);
-        //printf("\n\n---------> str1=%d: COMPARED: %s vs %s\n\n", str1, symTabItems[i].itemName, itemName);
-
-        int str2 = strcmp(symTabItems[i].scope,scope);
-        //printf("\n\n---------> str2=%d: COMPARED %s vs %s\n\n", str2, symTabItems[i].itemName, itemName);
-
-        if( str1 == 0 && (str2 == 0 || isGlobal == 0)) {
-            return 1; // found the ID in the table
-        }
-    }
-
-    return 0;
-}
-
-int getSymTabIndex() {
-    return symTabIndex; //Genius method for index retrieval: copyright Evan Kliwer
-}
-
-// Adds value to an ID in the symbol table
-int setItemValue(char itemName[50], char itemValue[8], char scope[50]) {
-	
-	for (int i = 0; i < SYMTAB_SIZE; i++) {
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-		int str1 = strcmp(symTabItems[i].itemName, itemName); 
-		int str2 = strcmp(symTabItems[i].scope,scope);
-		
-		if (str1 == 0 && (isGlobal == 0 || str2 == 0)) {
-			strcpy(symTabItems[i].itemValue, itemValue);
-			printf("\nSetting value of %s to %s ---->\n\n", symTabItems[i].itemName, itemValue);
-			showSymTable();
-			return 1; //value added 
-		}
-	}
-
-	printf("ERROR: Item %s was not found\n", itemName);	
-	return 0;
-}
-
-int setItemUsed(char itemName[50],  char scope[50]) {
-	
-	for (int i = 0; i < SYMTAB_SIZE; i++) {
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-		int str1 = strcmp(symTabItems[i].itemName, itemName); 
-		int str2 = strcmp(symTabItems[i].scope,scope);
-		
-		if (str1 == 0 && (isGlobal == 0 || str2 == 0)) {
-			symTabItems[i].itemUsed = 1;
-			showSymTable();
-			return 1; //value added 
-		}
-	}
-
-	printf("Item %s was not found\n", itemName);	
-	return 0;
-}
-
-// Returns the value of a given ID
-char *getValue(char itemName[50], char scope[50]) {
-
-	for(int i=0; i<100-1; i++) { 
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-		int str1 = strcmp(symTabItems[i].itemName, itemName);
-		int str2 = strcmp(symTabItems[i].scope,scope);
-
-		//If the variable is referenced in the wrong scope, we print a message and then cry
-		if (str1 == 0 && str2 == 1){
-			printf("ERROR: variable %s does not exist in this scope %s.",itemName,scope);
-		}
-
-		if( str1 == 0 && (str2 == 0 || isGlobal == 0)){
-			// printf("Item Value:%s\n", symTabItems[i].itemValue);
-			return symTabItems[i].itemValue;
-		}
-	}
-
-	return "NULL"; //item not found;
-}
-
-// Returns the ID of a given itemName
-int getItemID(char itemName[50], char scope[50]) {
-
-	for(int i=0; i<100; i++) {
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-		int str1 = strcmp(symTabItems[i].itemName, itemName);
-		int str2 = strcmp(symTabItems[i].scope, scope);
-		if( str1 == 0 && (str2 == 0 || isGlobal == 0)){
-			return symTabItems[i].itemID;
-		}
-	}
-
-	return -1; //item not found
-}
-
-char* getVariableType(char itemName[50], char scope[50]){
-    //char *name = "int";
-    //return name;
-    for(int i=0; i<SYMTAB_SIZE; i++){
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
-        int str1 = strcmp(symTabItems[i].itemName, itemName);
-        //printf("\n\n---------> str1=%d: COMPARED: %s vs %s\n\n", str1, symTabItems[i].itemName, itemName);
-        int str2 = strcmp(symTabItems[i].scope,scope);
-        //printf("\n\n---------> str2=%d: COMPARED %s vs %s\n\n", str2, symTabItems[i].itemName, itemName);
-        if( str1 == 0 && (str2 == 0 || isGlobal == 0)){
-			// printf("Debug: %s\n\n", symTabItems[i].itemType);
-            return symTabItems[i].itemType; 
-        }
-    }
-    return NULL;// ID not found in the table
-}
-
-char* getItemKind(char itemName[50], char scope[50]){
-    //char *name = "int";
-    //return name;
-    for(int i=0; i<SYMTAB_SIZE; i++){
-
-		int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+    // Checks if an identifier exists in the symbol table
+    for (int i = 0; i < SYMTAB_SIZE; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
         int str1 = strcmp(symTabItems[i].itemName, itemName);
         int str2 = strcmp(symTabItems[i].scope, scope);
 
-        if(str1 == 0) {
-            return symTabItems[i].itemKind; 
+        if (str1 == 0 && (str2 == 0 || isGlobal == 0)) {
+            return 1; // Identifier found
         }
     }
-    return NULL;// ID not found in the table
+    return 0; // Identifier not found
 }
 
-int compareTypes(char itemName1[50], char itemName2[50], char scope[50]){
+int getSymTabIndex() {
+    // Returns the current index of the symbol table
+    return symTabIndex;
+}
+
+int setItemValue(char itemName[50], char itemValue[8], char scope[50]) {
+    // Sets the value of a specific identifier in the symbol table
+    for (int i = 0; i < SYMTAB_SIZE; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0 && (isGlobal == 0 || str2 == 0)) {
+            strcpy(symTabItems[i].itemValue, itemValue);
+            printf("\nSetting value of %s to %s ---->\n\n", itemName, itemValue);
+            return 1; // Value set successfully
+        }
+    }
+    printf("ERROR: Item %s was not found\n", itemName);
+    return 0; // Identifier not found
+}
+
+int setItemUsed(char itemName[50], char scope[50]) {
+    // Marks an identifier as used in the symbol table
+    for (int i = 0; i < SYMTAB_SIZE; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0 && (isGlobal == 0 || str2 == 0)) {
+            symTabItems[i].itemUsed = 1;
+            return 1; // Identifier marked as used
+        }
+    }
+    printf("Item %s was not found\n", itemName);
+    return 0; // Identifier not found
+}
+
+char* getValue(char itemName[50], char scope[50]) {
+    // Retrieves the value of a given identifier
+    for (int i = 0; i < 100 - 1; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0 && str2 == 1) {
+            printf("ERROR: variable %s does not exist in this scope %s.", itemName, scope);
+        }
+        if (str1 == 0 && (str2 == 0 || isGlobal == 0)) {
+            return symTabItems[i].itemValue;
+        }
+    }
+    return "NULL"; // Identifier not found
+}
+
+int getItemID(char itemName[50], char scope[50]) {
+    // Retrieves the ID of a given identifier
+    for (int i = 0; i < 100; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0 && (str2 == 0 || isGlobal == 0)) {
+            return symTabItems[i].itemID;
+        }
+    }
+    return -1; // Identifier not found
+}
+
+char* getVariableType(char itemName[50], char scope[50]) {
+    // Retrieves the type of a given identifier
+    for (int i = 0; i < SYMTAB_SIZE; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0 && (str2 == 0 || isGlobal == 0)) {
+            return symTabItems[i].itemType;
+        }
+    }
+    return NULL; // Identifier not found
+}
+
+char* getItemKind(char itemName[50], char scope[50]) {
+    // Retrieves the kind of a given identifier
+    for (int i = 0; i < SYMTAB_SIZE; i++) {
+        int isGlobal = strcmp(symTabItems[i].scope, "GLOBAL");
+        int str1 = strcmp(symTabItems[i].itemName, itemName);
+        int str2 = strcmp(symTabItems[i].scope, scope);
+
+        if (str1 == 0) {
+            return symTabItems[i].itemKind;
+        }
+    }
+    return NULL; // Identifier not found
+}
+
+int compareTypes(char itemName1[50], char itemName2[50], char scope[50]) {
+    // Compares the types of two identifiers
     const char* idType1 = getVariableType(itemName1, scope);
     const char* idType2 = getVariableType(itemName2, scope);
-    
+
     printf("%s = %s\n", idType1, idType2);
-    
-    int typeMatch = strcmp(idType1, idType2);
-    if(typeMatch == 0){
-        return 1; // types are matching
-    }
-    else return 0;
+
+    return strcmp(idType1, idType2) == 0 ? 1 : 0; // Returns 1 if types match, otherwise 0
 }
